3a88e2100b59d1a07bab9848f116129f43534039
Simon Katz <sk@nomistech.com>
2020-03-30 23:33:48 +0100

    Do `(tnr/set-refresh-dirs "dev" "src" "test")`

--- dev/dev.clj
+++ dev/dev.clj
@@ -1,25 +1,20 @@
 (ns dev
-  (:require [clojure.java.classpath :as classpath]
-            [clojure.java.javadoc :refer [javadoc]]
+  (:require [clojure.java.javadoc :refer [javadoc]]
             [clojure.pprint :refer [pp pprint]]
             [clojure.repl :refer [apropos dir doc find-doc pst source]]
-            [clojure.tools.namespace.repl :as tnr])
-  (:import (java.io File)))
+            [clojure.tools.namespace.repl :as tnr]))
 
 ;;;; ___________________________________________________________________________
-;;;; Fix classpath directories for tools.namspace.
 
-;;;; I have found that `refresh` does nothing.
-;;;; It seems:
-;;;; - `(classpath/classpath)` does not end up calling
-;;;;   `(classpath/system-classpath)`.
-;;;; - That's because
-;;;;   `(classpath/classpath (clojure.lang.RT/baseLoader))`
-;;;;   returns a File for "/Library/Java/JavaVirtualMachines/adoptopenjdk-13.0.1.jdk/Contents/Home/lib/src.zip".
-;;;; - I've never seen this problem before.
-;;;;   Might it be related to jenv? (Just a guess.)
+;;;; `clojure.tools.namespace.repl` sometimes doesn't work out what dirs to
+;;;; read from when refreshing.
+;;;;
+;;;; See:
+;;;; - https://ask.clojure.org/index.php/8288/java-11-and-tools-namespace
+;;;; - https://github.com/clojure-emacs/cider/issues/2686
+;;;; - https://github.com/clojure-emacs/cider/issues/2686#issuecomment-532389499
+;;;;
+;;;; Conclusion:
+;;;; - Do the following:
 
-(let [dirs-on-classpath (filter #(.isDirectory ^File %)
-                                (classpath/system-classpath))]
-  (apply tnr/set-refresh-dirs
-         dirs-on-classpath))
+(tnr/set-refresh-dirs "dev" "src" "test")
